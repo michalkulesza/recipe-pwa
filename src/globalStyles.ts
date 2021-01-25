68fb688a6615086a5ecf640510ae29c6e024d018
Michal <kulesza.michal@gmail.com>
2021-01-25 17:09:41 +0100

    Cleanup and basic setup

---
+++ src/globalStyles.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+html, body {
+   font-family: 'Open Sans', sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   background-color: #141414;
+   color: #fff;
+   font-size: 16px;
+   overflow-x: hidden;
+   
+   }
+
+ul,li{
+   margin: 0;
+   padding: 0;
+   list-style: none;
+}
+
+input{
+   padding: 0;
+	border: 0;
+}
+
+button{
+   cursor: pointer;
+   border: none;
+   &:focus{
+      outline: none;
+   }
+}
+
+*{
+   box-sizing: border-box;
+   user-select: none;
+}
+
+a{
+   text-decoration: none;
+   color: #fff
+}
+`;
+
+export default GlobalStyle;
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyles from "./globalStyles";
+import ReactDOM from "react-dom";
+import "normalize.css";
+
+import App from "./App";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<GlobalStyles>
+			<App />
+		</GlobalStyles>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
