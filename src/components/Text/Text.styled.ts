8a951f485c88c9524237295328fe9cb0332ed91a
Michal <kulesza.michal@gmail.com>
2021-01-26 10:10:58 +0100

    + Text component

---
+++ src/components/Text/Text.styled.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components/macro";
+import { css } from "styled-components";
+import theme from "../../styles/theme";
+import { ColorsI } from "../../types";
+
+interface PropsI {
+	inverted: boolean;
+	color: keyof ColorsI;
+}
+
+const commonStyles = ({ inverted, color }: PropsI) => css`
+	color: ${inverted ? theme.textInverted : theme[color]};
+`;
+
+export const H1 = styled.h1<PropsI>`
+	${props => commonStyles(props)};
+	font-size: 28pt;
+	font-weight: normal;
+`;
+
+export const H1B = styled.h1<PropsI>`
+	${props => commonStyles(props)};
+	font-size: 28pt;
+	font-weight: bold;
+`;
+
+export const H2B = styled.h1<PropsI>`
+	${props => commonStyles(props)};
+	font-size: 21pt;
+	font-weight: bold;
+`;
+
+export const H3 = styled.h1<PropsI>`
+	${props => commonStyles(props)};
+	font-size: 13pt;
+	font-weight: normal;
+`;
+
+export const H3B = styled.h1<PropsI>`
+	${props => commonStyles(props)};
+	font-size: 13pt;
+	font-weight: bold;
+`;
+
+export const H4 = styled.h1<PropsI>`
+	${props => commonStyles(props)};
+	font-size: 11pt;
+	font-weight: normal;
+`;
+
+export const H4B = styled.h1<PropsI>`
+	${props => commonStyles(props)};
+	font-size: 11pt;
+	font-weight: bold;
+`;
---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { ColorsI } from "../../types";
+
+import { H1, H1B, H2B, H3, H3B, H4, H4B } from "./Text.styled";
+
+interface Props {
+	children: React.ReactNode;
+	type?: "H1" | "H1Bold" | "H2Bold" | "H3" | "H3Bold" | "H4" | "H4Bold";
+	inverted?: boolean;
+	color?: keyof ColorsI;
+}
+
+const Text: React.FC<Props> = ({ children, type = "H3", inverted = false, color = "text" }) => {
+	return type === "H1" ? (
+		<H1 inverted={inverted} color={color}>
+			{children}
+		</H1>
+	) : type === "H1Bold" ? (
+		<H1B inverted={inverted} color={color}>
+			{children}
+		</H1B>
+	) : type === "H2Bold" ? (
+		<H2B inverted={inverted} color={color}>
+			{children}
+		</H2B>
+	) : type === "H3" ? (
+		<H3 inverted={inverted} color={color}>
+			{children}
+		</H3>
+	) : type === "H3Bold" ? (
+		<H3B inverted={inverted} color={color}>
+			{children}
+		</H3B>
+	) : type === "H4" ? (
+		<H4 inverted={inverted} color={color}>
+			{children}
+		</H4>
+	) : (
+		<H4B inverted={inverted} color={color}>
+			{children}
+		</H4B>
+	);
+};
+
+export default Text;
